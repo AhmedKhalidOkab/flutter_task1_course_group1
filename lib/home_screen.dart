e8c6c49b2c6717cc638ff6fa19d6b579d7faeb4a
hamada mostafa <93394629+hamada6102000@users.noreply.github.com>
2022-09-29 21:28:56 +0200

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,85 +5,71 @@ class Homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.white24,
         elevation: 0,
       ),
       body: SingleChildScrollView(
         child: Container(
           width: double.infinity,
-          padding: EdgeInsets.only(top: 50, left: 30, right: 30),
+          padding: EdgeInsets.only(top: 50, bottom: 60),
           child: Card(
-              shadowColor: Colors.white54,
-              elevation: 5,
+              shadowColor: Colors.white60,
+              elevation: 3,
               child: Container(
                 padding: EdgeInsets.all(20),
                 child: Column(
                   children: [
                     const Text(
-                      'Register',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                      'Sign Up                                                                   ',
+                      style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
                     ),
                     const SizedBox(
-                      height: 15,
+                      height: 50,
                     ),
                     TextFormField(
                       decoration: const InputDecoration(
-                        icon: Icon(Icons.person),
-                        label: Text('Full Name'),
-                      ),
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.email),
-                        label: Text('E-mail'),
-                      ),
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                          suffixIcon: Icon(Icons.visibility_off),
-                          icon: Icon(Icons.key_sharp),
-                          label: Text('Password')),
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        suffixIcon: Icon(Icons.visibility_off),
-                        icon: Icon(Icons.key_sharp),
-                        label: Text('confirm Password'),
+                        label: Text('Name'),
                       ),
                     ),
                     const SizedBox(
-                      height: 25,
+                      height: 35,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Email'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 35,
+                    ),
+                    TextFormField(
+                      decoration:
+                          const InputDecoration(label: Text('Password')),
+                    ),
+                    const SizedBox(
+                      height: 60,
                     ),
                     Container(
+                      //color: Colors.black,
                       width: double.infinity,
                       height: 60,
                       child: ElevatedButton(
                           style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  Color.fromRGBO(0, 200, 0, 150)),
                               shape: MaterialStateProperty.all(
                                   RoundedRectangleBorder(
                                       borderRadius:
-                                          BorderRadius.circular(18.0)))),
+                                          BorderRadius.circular(5.0)))),
                           onPressed: () {},
                           child: const Text(
-                            'Register ',
+                            'SIGN UP',
                             style: TextStyle(
                                 fontSize: 18, fontWeight: FontWeight.w500),
                           )),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Aleardy Register ?',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w400),
-                        ),
-                        TextButton(onPressed: () {}, child: Text('Login'))
-                      ],
                     ),
                   ],
                 ),
