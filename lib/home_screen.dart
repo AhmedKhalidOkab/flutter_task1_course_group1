c124a49461fe1771fdcacd3a4dd162ade715bdab
hamada mostafa <93394629+hamada6102000@users.noreply.github.com>
2022-10-03 21:57:15 +0200

    task2

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,80 +1,76 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class Homescreen extends StatelessWidget {
+class Homescreen extends StatefulWidget {
+  Homescreen({Key? key}) : super(key: key);
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  var text = "blue";
+  var colour = Colors.blue;
+  var colour2 = Colors.red;
+  var colour3 = Colors.black;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white24,
-        elevation: 0,
+        title: Text('$text  app bar'),
+        backgroundColor: colour,
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          padding: EdgeInsets.only(top: 50, bottom: 60),
-          child: Card(
-              shadowColor: Colors.white60,
-              elevation: 3,
-              child: Container(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    const Text(
-                      'Sign Up                                                                   ',
-                      style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        label: Text('Name'),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 35,
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        label: Text('Email'),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 35,
-                    ),
-                    TextFormField(
-                      decoration:
-                          const InputDecoration(label: Text('Password')),
-                    ),
-                    const SizedBox(
-                      height: 60,
-                    ),
-                    Container(
-                      //color: Colors.black,
-                      width: double.infinity,
-                      height: 60,
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all(
-                                  Color.fromRGBO(0, 200, 0, 150)),
-                              shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(5.0)))),
-                          onPressed: () {},
-                          child: const Text(
-                            'SIGN UP',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w500),
-                          )),
-                    ),
-                  ],
-                ),
-              )),
+      body: Center(
+          child: Container(
+        width: 250,
+        height: 40,
+        color: Colors.black,
+        child: Row(
+          children: [
+            Container(
+              color: colour,
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      colour = Colors.red;
+
+                      text = 'Red';
+                    });
+                  },
+                  child: const Text(
+                    'Red',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 248, 12, 8), fontSize: 20),
+                  )),
+            ),
+            SizedBox(
+                width: 20,
+                child: Container(
+                  color: Colors.white,
+                )),
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    colour = Colors.red;
+                    colour3 = Colors.red;
+                  });
+                },
+                child: const Text(
+                  "   change both",
+                  style: TextStyle(color: Colors.red, fontSize: 20),
+                ))
+          ],
         ),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            colour = Colors.blue;
+            text = 'Blue';
+          });
+        },
+        backgroundColor: Colors.blue,
+        child: const Text("back"),
       ),
     );
   }
